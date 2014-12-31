eeac723479ec6c5a4bd59d23e7db7db1ece0e6a6
Friedrich Brunzema <Friedrich.Brunzema@absciex.com>
2014-12-31 15:49:22 -0500

    Ran the project creation wizard

---
+++ MakeFriendAssembly/MakeFriendAssembly/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace DreamWorks.MakeFriendAssembly
+{
+    static class GuidList
+    {
+        public const string guidMakeFriendAssemblyPkgString = "92bace77-fb0d-44b2-bc4c-943893fc18c0";
+        public const string guidMakeFriendAssemblyCmdSetString = "59933e76-236c-4368-83cf-c819108f1c50";
+
+        public static readonly Guid guidMakeFriendAssemblyCmdSet = new Guid(guidMakeFriendAssemblyCmdSetString);
+    };
+}
---
+++ MakeFriendAssembly/MakeFriendAssembly/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace DreamWorks.MakeFriendAssembly
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidMakeFriendAssembly =        0x100;
+
+
+    };
+}
