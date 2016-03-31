d32cc3074e04773375e17c321fe3bbb468123845
Friedrich Brunzema <Friedrich.Brunzema@absciex.com>
2016-03-31 17:38:09 -0400

    VSIX was not installing; updated guid of package

--- MakeFriendAssembly/Guids.cs
+++ MakeFriendAssembly/Guids.cs
@@ -6,7 +6,7 @@ namespace DreamWorks.MakeFriendAssembly
 {
     static class GuidList
     {
-        public const string guidMakeFriendAssemblyPkgString = "92bace77-fb0d-44b2-bc4c-943893fc18c0";
+        public const string guidMakeFriendAssemblyPkgString = "92bace77-fb0d-44b2-bc4c-943893fc18c1";
         public const string guidMakeFriendAssemblyCmdSetString = "59933e76-236c-4368-83cf-c819108f1c50";
 
         public static readonly Guid guidMakeFriendAssemblyCmdSet = new Guid(guidMakeFriendAssemblyCmdSetString);
